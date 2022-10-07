8fa75879b0c2c7947cf47a65219148ca885e8da0
CHNTYDRMDHN <chintyadwi910@gmail.com>
2022-10-07 22:04:46 +0700

    di baca ya

---
+++ phpdasar.php
@@ -0,0 +1,29 @@
+<?php
+$hallo_tia = "saya siswa smkn 1 karawang";
+echo $hallo_tia;
+echo "<br />";
+echo "<br />";
+echo("hallo saya chintya");
+echo "<br />";
+echo strlen("halo saya chintya");
+echo "<br />";
+//menghitung panjang string
+echo("sekolah di smkn 1 karawang");
+echo "<br />";
+echo str_word_count("sekolah di smkn 1 karawang");
+echo "<br />";
+//menghitung jumlah kata
+echo("di jurusan");
+echo "<br />";
+echo strrev ("di jurusan");
+echo "<br />";
+//membalikan string
+echo("rekayasa perangkat lunak");
+echo "<br />";
+echo strpos("rekayasa perangkat lunak", "lunak");
+echo "<br />";
+//mencari teks tertentu dalam string
+echo str_replace("SMKN 1 KARAWANG", "neskar", $hallo_tia);
+echo "<br />";
+//mengganti bebrapa karakter dengan beberapa karakter lain dalam sebuah string
+?>
